86ea41b1f1a5070866e89f6a7b3c292833372039
Gyuray Kamber <gyuraykamber99@gmail.com>
2025-05-16 17:49:40 +0000

    Enable and configure your preview environment If you use a template, previews are often already configured for you. If previews aren't already set up in your template, you can configure them in the project's Nix configuration file.
    From your workspace, open .idx/dev.nix.
    
    Firebase Studio automatically generates this file when you create a new workspace and includes any applicable preview environments based on the template you select. If the file isn't in your Firebase Studio code repository, create it and then set the idx.previews attribute to true. Then, add configuration attributes, as the following example shows:
    
    { pkgs, ... }: {
    
      # NOTE: This is an excerpt of a complete Nix configuration example.
      # For more information about the dev.nix file in Firebase Studio, see
      # https://firebase.google.com/docs/studio/customize-workspace
    
      # Enable previews and customize configuration
      idx.previews = {
        enable = true;
        previews = {
          # The following object sets web previews
          web = {
            command = [
              "npm"
              "run"
              "start"
              "--"
              "--port"
              "$PORT"
              "--host"
              "0.0.0.0"
              "--disable-host-check"
            ];
            manager = "web";
            # Optionally, specify a directory that contains your web app
            # cwd = "app/client";
          };
          # The following object sets Android previews
          # Note that this is supported only on Flutter workspaces
          android = {
            manager = "flutter";
          };
        };
      };
    }
    For a full list of Nix attributes in Firebase Studio, see Nix + Firebase Studio.
    
    Rebuild your environment:
    
    From the command palette (Cmd+Shift+P/Ctrl+Shift+P), run the Firebase Studio: Hard restart command.
    From the Environment config updated notification, click Rebuild environment.
    When you rebuild the environment after modifying your dev.nix file, the preview panel appears in your workspace showing either or both of Android and Web tabs, depending on what you've enabled.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,43 +1,35 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_20
-    pkgs.zulu
-  ];
-  # Sets environment variables in the workspace
-  env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
-  services.firebase.emulators = {
-    detect = true;
-    projectId = "demo-app";
-    services = ["auth" "firestore"];
-  };
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-    ];
-    workspace = {
-      onCreate = {
-        default.openFiles = [
-          "src/app/page.tsx"
-        ];
-      };
-    };
-    # Enable previews and customize configuration
+{ pkgs, ... }: {
+
+  # NOTE: This is an excerpt of a complete Nix configuration example.
+  # For more information about the dev.nix file in Firebase Studio, see
+  # https://firebase.google.com/docs/studio/customize-workspace
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
     previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
-          manager = "web";
-        };
+      # The following object sets web previews
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+        # Optionally, specify a directory that contains your web app
+        # cwd = "app/client";
       };
+      # The following object sets Android previews
+      # Note that this is supported only on Flutter workspaces
+      # android = {
+      #   manager = "flutter";
+      # };
     };
   };
 }
